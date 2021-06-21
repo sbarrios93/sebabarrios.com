d4bbb8b773e75d4ead7546cac41b0ed6fbc8fb40
Sebastian Barrios Slight <sbarrios93@gmail.com>
2021-06-21 18:02:16 -0400

    content change

--- .eleventy.js
+++ .eleventy.js
@@ -305,7 +305,7 @@ module.exports = function (eleventyConfig) {
   });
 
   return {
-    templateFormats: ["md", "njk", "html", "liquid"],
+    templateFormats: ["md", "njk", "html", "liquid", "css"],
 
     // If your site lives in a different subdirectory, change this.
     // Leading or trailing slashes are all normalized away, so don’t worry about those.
