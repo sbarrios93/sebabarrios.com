7a38d5323dab3c1befd3f9b56cec71da5b5880aa
sbarrios93 <sbarrios93@gmail.com>
2021-06-22 20:03:53 -0400

    include bash script to update _site

---
+++ copy_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Start building site"
+npm run build
+echo "Remove _site folder from _site-branch"
+rm -r _site-branch/_site
+echo "Start copy"
+cp -R _site _site-branch
+echo "End Copy"
+cd _site-branch
+git add .
+git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"
+git push
