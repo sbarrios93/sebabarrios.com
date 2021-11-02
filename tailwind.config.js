3cc90ba5e230ac88c17811d313df269f17b3b3ae
sbarrios93 <sbarrios93@gmail.com>
2021-11-02 15:54:12 -0500

    fix?

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['_site/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Inter var", "Inter", 'ui-sans-serif', 'system-ui', 'Arial', 'sans-serif']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
