3162a64fafb8700f9999412dc7465fed8cae8ee6
sbarrios93 <sbarrios93@gmail.com>
2022-01-02 12:32:58 -0600

    layout aliases

--- .eleventy.js
+++ .eleventy.js
@@ -1,31 +1,32 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
+module.exports = function (eleventyConfig) {
+	// Layout aliases
+  eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
+	eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-module.exports = function(eleventyConfig) {
+  //
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+	eleventyConfig.addPassthroughCopy("images");
+	eleventyConfig.addPassthroughCopy("admin");
 
-  eleventyConfig.addPassthroughCopy('images')
-  eleventyConfig.addPassthroughCopy('admin')
+	const { DateTime } = require("luxon");
 
-  const {
-    DateTime
-  } = require("luxon");
+	// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {
+			zone: "utc",
+		}).toFormat("yy-MM-dd");
+	});
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-      return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
-      }).toFormat('yy-MM-dd');
-    });
+	eleventyConfig.addFilter("readableDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj, {
+			zone: "utc",
+		}).toFormat("dd-MM-yy");
+	});
 
-    eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat("dd-MM-yy");
-  });
-
-  return {
-    dir: { input: 'src', output: '_site' }
-  };
+	return {
+		dir: { input: "src", output: "_site" },
+	};
 };
